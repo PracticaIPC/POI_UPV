4507579a82097e327cf146c96285be9a097ab38e
User <User@DESKTOP-1JSHHM8>
2022-04-05 16:40:35 +0200

    IPC

---
+++ src/poiupv/InicioSesionController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poiupv;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class InicioSesionController implements Initializable {
+
+    @FXML
+    private CheckBox mostrarfxID;
+    @FXML
+    private TextField usuariofxID;
+    @FXML
+    private Button cancelarfxID;
+    @FXML
+    private Button continuarfxID;
+    @FXML
+    private DatePicker fechafxID;
+    @FXML
+    private Button seleccionfxID;
+    @FXML
+    private ImageView imagenfxID;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void bMostrar(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void bUsuario(ActionEvent event) {
+    }
+
+    @FXML
+    private void bCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void bContinuar(ActionEvent event) {
+    }
+
+    @FXML
+    private void bFecha(ActionEvent event) {
+    }
+
+    @FXML
+    private void bSelección(ActionEvent event) {
+    }
+    
+}
--- src/poiupv/PantallaInicialController.java
+++ src/poiupv/PantallaInicialController.java
@@ -7,14 +7,24 @@ package poiupv;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
  *
  * @author User
  */
-public class InicioSesionFXMLController implements Initializable {
+public class PantallaInicialController implements Initializable {
+
+    @FXML
+    private Button registrofxID;
+    @FXML
+    private Button incioSesionfxID;
+    @FXML
+    private Button invitadofxID;
 
     /**
      * Initializes the controller class.
@@ -23,5 +33,17 @@ public class InicioSesionFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void bRegistro(ActionEvent event) {
+    }
+
+    @FXML
+    private void bInicioSesion(ActionEvent event) {
+    }
+
+    @FXML
+    private void bInvitado(ActionEvent event) {
+    }
     
 }
