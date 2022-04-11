952be646609a6c1156e1f9ea3db6ed997d14e9dd
User <User@DESKTOP-1JSHHM8>
2022-04-11 18:04:05 +0200

    IPC

--- src/poiupv/RegistroController.java
+++ src/poiupv/RegistroController.java
@@ -17,9 +17,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -48,13 +52,15 @@ public class RegistroController implements Initializable {
     private Button seleccionfxID;
     @FXML
     private ImageView imagenfxID;
+    @FXML
+    private Label InicioSesionfxID;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        InicioSesionfxID.setTextFill(Color.rgb(107, 207, 176));
     }    
 
     @FXML
@@ -75,6 +81,36 @@ public class RegistroController implements Initializable {
 
     @FXML
     private void bSelección(ActionEvent event) {
+    }
+
+    @FXML
+    private void bInicioSesion(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("InicioSesion.fxml"));
+        Parent root = loader.load();
+        
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setScene(scene);
+        stage.show();
+        stage.setTitle("Inicio Sesión");
+        
+        Stage myStage = (Stage) this.InicioSesionfxID.getScene().getWindow();
+            myStage.close();
+        
+    }
+
+    
+    
+
+    @FXML
+    private void finInicio(MouseEvent event) {
+        InicioSesionfxID.setTextFill(Color.rgb(38, 159, 121));
+    }
+
+    @FXML
+    private void arrastreInicio(MouseEvent event) {
+        InicioSesionfxID.setTextFill(Color.rgb(36, 73, 62));
     }
     
 }
